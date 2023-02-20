bf078425abb2507b292918ac88f19e19321239ee
NurbekSaitqulov03 <nurbek.saitqulov23@gmail.com>
2023-02-20 20:16:19 +0500

    get_post_month.py

--- get_post_month.py
+++ get_post_month.py
@@ -12,5 +12,21 @@ def get_post_month(data:dict,month:int)->int:
     Returns: 
         int: the number of posts for the given month
     """
+    x = 0
+    for i in data['messages']:
+        date_year = i['date'].split('T')
+        date_month = date_year[0].split('-')
+        
+        if (date_month[1][1]).isdigit():
+            
+            if int(date_month[1][0])==0:
+                if int(date_month[1][1])==month:
+                    x += 1
+
+            if int(date_month[1])==month:
+                x += 1 
+
+    return x
     
-    return
+data = fromJson('data/result.json')
+print(get_post_month(data, 11))
