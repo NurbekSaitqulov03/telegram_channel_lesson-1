44f69967a71cc5f8a105bea7848f4b17887df52e
NurbekSaitqulov03 <nurbek.saitqulov23@gmail.com>
2023-02-20 16:16:15 +0500

    get_number_of_posts.py

--- get_number_of_posts.py
+++ get_number_of_posts.py
@@ -10,4 +10,12 @@ def get_number_of_posts(data:dict)->int:
     Returns: 
         int: the number of posts for the given dictionary
     """
-    return
+    x = 0
+    
+    for i in data['messages']:
+        if i['type']=='message':
+            x += 1
+
+    return x
+data = fromJson('data/result.json')
+print(get_number_of_posts(data))
